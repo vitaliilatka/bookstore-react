c73fb5fbfeea3259557990b2ce72576b3644e057
vlatka <latka.vitalii@gmail.com>
2023-07-29 02:01:53 +0200

    adding About component

---
+++ src/components/About.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const About = () => {
+    return (
+        <div>
+            <h2>About Us</h2>
+            <p>
+                Welcome to our online bookstore! We offer a wide selection of books on various topics, from fiction to non-fiction. Our mission is to provide book lovers with access to a diverse collection of books from around the world.
+            </p>
+            <p>
+                If you have any questions or need assistance, please feel free to contact us at support@bookstore.com.
+            </p>
+        </div>
+    );
+};
+
+export default About;
