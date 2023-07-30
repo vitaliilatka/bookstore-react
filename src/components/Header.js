fe450a90179c7706b02e4f8abee24955804f8419
vlatka <latka.vitalii@gmail.com>
2023-07-30 03:20:50 +0200

    adding header

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header>
+            <h1>My BookStore</h1>
+            {/* Add logo */}
+        </header>
+    );
+};
+
+export default Header;
