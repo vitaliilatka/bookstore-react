73df4ceee0fd05c868418f41dfc1514c60fdb0e9
vlatka <latka.vitalii@gmail.com>
2023-07-30 12:33:05 +0200

    adding component Books.js

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,24 +1,25 @@
 import React from 'react';
-
-const booksData = [
-    {
-        id: 1, title: 'Book 1', autor: 'Autor 1', price: 10.99
-    },
-    {
-        id: 2, title: 'Book 2', autor: 'Autor 2', price: 12.99
-    },
-];
+import { Link } from 'react-router-dom';
 
 const Books = () => {
+    const books = [
+        {
+            id: 1, title: 'Book 1', autor: 'Autor 1', price: 10.99
+        },
+        {
+            id: 2, title: 'Book 2', autor: 'Autor 2', price: 12.99
+        },
+    ];
     return (
         <div>
-            <h2>Books</h2>
+            <h2>Avaliable Books</h2>
             <ul>
-                {booksData.map((book) => (
+                {books.map((book) => (
                     <li key={book.id}>
                         <h3>{book.title}</h3>
                         <p>Author: {book.autor}</p>
                         <p>Price: ${book.price}</p>
+                        <Link to={`/books/${bbok.id}`}>View Details</Link>
                         {/* {Add button 'Add to cart', and router for it} */}
                     </li>
                 ))}
