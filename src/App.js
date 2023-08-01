3ebed488ef1b81caf393f7544ce0d61fcc935adf
vlatka <latka.vitalii@gmail.com>
2023-08-01 02:56:20 +0200

    fixing bug

--- src/App.js
+++ src/App.js
@@ -1,28 +1,23 @@
 import React from 'react';
 import './styles.css';
-import { Route } from 'react-router-dom';
+// import { BrowserRouter as Router, Route } from 'react-router-dom';
 // import { Provider } from './context/Context';
 import Navbar from './components/Navbar';
 import Header from './components/Header';
 import Books from './components/Books';
 import About from './components/About';
-import BookCart from './components/BookCart';
-import BookDetails from './components/BookDetails';
-import Search from './components/Search';
-import BookCartCheckOut from './components/BookCartCheckOut';
+// import BookCart from './components/BookCart';
+// import BookDetails from './components/BookDetails';
+// import Search from './components/Search';
+// import BookCartCheckOut from './components/BookCartCheckOut';
 
 function App() {
   return (
-    <div className='App'>
-      <Header />
+    <div>
       <Navbar />
-      <Search />
-      <Route exact path="/" component={Books} />
-      <Route path="/books/:id" component={BookDetails} />
-      <Route path="/about" component={About} />
-      <Route path="/cart" component={BookCart} />
-      <Route path='/checkout' component={BookCartCheckOut} />
-      <Route path="/book-details/:id" component={BookDetails} />
+      <Header />
+      <Books />
+      <About />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+// import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter>
+  <BrowserRouter>
+    <React.StrictMode>
       <App />
-    </BrowserRouter>
-  </React.StrictMode>
+    </React.StrictMode>
+  </BrowserRouter>
 );
 
