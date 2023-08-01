d2816d18d8435650e1405c506bbc96614d727446
vlatka <latka.vitalii@gmail.com>
2023-08-01 13:42:53 +0200

    adding styles modules Header and Navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import React from 'react';
 import './styles.css';
-// import { BrowserRouter as Router, Route } from 'react-router-dom';
 // import { Provider } from './context/Context';
 import Navbar from './components/Navbar';
 import Header from './components/Header';
 import Books from './components/Books';
 import About from './components/About';
-// import BookCart from './components/BookCart';
-// import BookDetails from './components/BookDetails';
-// import Search from './components/Search';
-// import BookCartCheckOut from './components/BookCartCheckOut';
+import BookCart from './components/BookCart';
+import BookDetails from './components/BookDetails';
+import Search from './components/Search';
+import BookCartCheckOut from './components/BookCartCheckOut';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div>
       <Navbar />
       <Header />
-      <Books />
-      <About />
+      <Search />
+      <Routes>
+        <Route path="/" element={<Books />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/cart" element={<BookCart />} />
+        <Route path="/books/:id" element={<BookDetails />} />
+        <Route path="/checkout" element={<BookCartCheckOut />} />
+      </Routes>
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import styles from '../styles/Navbar.module.css';
 
 const Navbar = () => {
     return (
-        <nav>
-            <ul>
-                <li>
-                    <Link to="/">Home</Link>
-                    <Link to="/about">About</Link>
-                    <Link to="/cart">Cart</Link>
-                </li>
-            </ul>
+        <nav className={styles.navbar}>
+            <div>
+                <Link className={styles.navLink} to="/">
+                    Home
+                </Link>
+                <Link className={styles.navLink} to="/about">
+                    About
+                </Link>
+                <Link className={styles.navLink} to="/cart">
+                    Cart
+                </Link>
+            </div>
         </nav>
     );
 };
