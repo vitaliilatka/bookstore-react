7df9b531e7f0c09b6bfdb49048a9ab0986b4e978
vlatka <latka.vitalii@gmail.com>
2023-08-03 15:55:44 +0200

    adding styles BookCartItem

--- src/components/BookCartItem.js
+++ src/components/BookCartItem.js
@@ -1,13 +1,13 @@
 import React from 'react';
+import styles from '../styles/BookCartItem.module.css';
 
 const BookCartItem = ({ book }) => {
-    const { title, author, price } = book;
 
     return (
-        <div>
-            <h3>{title}</h3>
-            <p>Author: {author}</p>
-            <p>Price: ${price.toFixed(2)}</p>
+        <div className={styles.BookCartItemContainer}>
+            <h3 className={styles.bookTitle}>{book.title}</h3>
+            <p className={styles.bookAuthor}>Author: {book.author}</p>
+            <p className={styles.bookPrice}>Price: ${book.price.toFixed(2)}</p>
             {/*Add button for changing books in cart and delete book */}
         </div>
     );
