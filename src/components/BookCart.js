a73f4030074bf5e3ca03348493d0aacf2818275d
vlatka <latka.vitalii@gmail.com>
2023-08-11 03:01:14 +0200

    trying remove button

--- src/components/BookCart.js
+++ src/components/BookCart.js
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { useCart } from "../context/CartContext";
 import BookCard from "./BookCard";
 import styles from '../styles/BookCart.module.css';
 
 const BookCart = () => {
     const { cartItems, removeFromCart } = useCart();
+    const [quantities, setQuantities] = useState([]);
 
-    const total = cartItems
-        .reduce((sum, book) => sum + parseFloat(book.price) * book.quantity, 0.0);
+    const handleQuantityChange = (id, quantity) => {
+        setQuantities((prevQuantities) => ({ ...prevQuantities, [id]: quantity }));
+    };
+
+    const calculateTotal = () => {
+        return cartItems.reduce((total, book) => {
+            const quantity = quantities[book.id] || 1;
+            return total + book.price * quantity;
+        }, 0);
+    };
+
+    // const total = cartItems
+    //     .reduce((sum, book) => sum + parseFloat(book.price) * book.quantity, 0.0);
 
     return (
         <div className={styles.bookCartContainer}>
@@ -19,7 +31,14 @@ const BookCart = () => {
                     {cartItems.map((book) => (
                         <BookCard key={book.id} book={book} onRemoveFromCart={() => removeFromCart(book.id)} />
                     ))}
-                    <p className={styles.totalPrice}>Total: ${total.toFixed(2)}</p>
+                    <input
+                        type="number"
+                        value={quantities[book.id] || 1}
+                        onChange={(e) => handleQuantityChange(book.id, e.target.value)}
+                        min="1"
+                    />
+                    <button onClick={() => removeFromCart(book.id)}>Remove</button>
+                    <p className={styles.totalPrice}>Total: ${calculateTotal().toFixed(2)} books</p>
                 </div>
             )}
         </div>
