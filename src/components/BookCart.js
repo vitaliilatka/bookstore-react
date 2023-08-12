54ec9b486b05a1b398745822f489c1abd6284838
vlatka <latka.vitalii@gmail.com>
2023-08-12 03:32:44 +0200

    returning to simple bookCart

--- src/components/BookCart.js
+++ src/components/BookCart.js
@@ -1,22 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 import { useCart } from "../context/CartContext";
 import BookCard from "./BookCard";
 import styles from '../styles/BookCart.module.css';
 
 const BookCart = () => {
     const { cartItems, removeFromCart } = useCart();
-    const [quantities, setQuantities] = useState([]);
-
-    const handleQuantityChange = (id, quantity) => {
-        setQuantities((prevQuantities) => ({ ...prevQuantities, [id]: quantity }));
-    };
-
-    const calculateTotal = () => {
-        return cartItems.reduce((total, book) => {
-            const quantity = quantities[book.id] || 1;
-            return total + book.price * quantity;
-        }, 0);
-    };
 
     // const total = cartItems
     //     .reduce((sum, book) => sum + parseFloat(book.price) * book.quantity, 0.0);
@@ -29,16 +17,16 @@ const BookCart = () => {
             ) : (
                 <div>
                     {cartItems.map((book) => (
-                        <BookCard key={book.id} book={book} onRemoveFromCart={() => removeFromCart(book.id)} />
+                        <BookCard
+                            key={book.id}
+                            book={book}
+                            showAddToCart={false}
+                            showRemove={true}
+                            onRemove={() => removeFromCart(book.id)}
+                        // onRemoveFromCart={() => removeFromCart(book.id)}
+                        />
                     ))}
-                    <input
-                        type="number"
-                        value={quantities[book.id] || 1}
-                        onChange={(e) => handleQuantityChange(book.id, e.target.value)}
-                        min="1"
-                    />
-                    <button onClick={() => removeFromCart(book.id)}>Remove</button>
-                    <p className={styles.totalPrice}>Total: ${calculateTotal().toFixed(2)} books</p>
+                    <p className={styles.totalPrice}>Total: books</p>
                 </div>
             )}
         </div>
